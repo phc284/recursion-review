85bcf76bb30d502595987291ea5fee15bbe3d1b0
Hack Reactor Student <student@hackreactor.com>
2017-08-14 11:48:25 -0500

    Checking for funcitons in objects

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,31 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  
+  if (Array.isArray(obj)) {
+    var newObj = obj.map(function (item) {
+      return stringifyJSON(item);
+    });
+    return '[' + newObj + ']';
+  } else if (typeof obj === 'object') {
+    var string = '';
+    if (obj === null) {
+      return 'null';
+    }
+    for (var key in obj) {
+      if(typeof obj[key] !== 'function'){
+        if(string !== ''){
+          string += ',';
+        }
+        string += stringifyJSON(key) + ':' + stringifyJSON(obj[key]);
+      } else {
+          return '{}';
+        }
+    }
+    return '{' + string + '}';
+  } else if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+
+  return '' + obj;
 };
