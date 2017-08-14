069e1cdc1bc7c4d9857d9f13477a23ddebf87557
Hack Reactor Student <student@hackreactor.com>
2017-08-14 15:20:31 -0500

    Resolved getElementsByClassName test case

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,16 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
+  let elements = [];
+  let recurseClassName = function (node) {
+    if (node.classList !== undefined && node.classList.contains(className)) {
+      elements.push(node);
+    }
+    for (let key = 0; key < node.childNodes.length; key++) {
+      recurseClassName(node.childNodes[key]);
+    }
+  };
+  recurseClassName(document.body);
+  return elements;
   // your code here
 };
